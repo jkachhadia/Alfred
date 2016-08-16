83526e071c502bf579236767b40f475244344612
jkachhadia <jaykachhadia@hotmail.com>
2016-08-16 21:39:01 +0530

    added the db

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import time
 import os
 import sys
 import requests
 import json
 from flask import Flask,request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app=Flask(__name__)
 app.config.from_pyfile('app.cfg')
@@ -12,13 +14,20 @@ db = SQLAlchemy(app)
 PAGE_ACCESS_TOKEN = "EAAEMH5yZAxtABAIi5RjBMuW5ttS3kgZCOIM1LM92nZBp6ZAMZCg1NPleNMmKsRfS2Jbb4FrYeXca1knaalZBLJf7UCGj3XfBCZBn5YW1w0ZBkY4zhOZASW5Uf75IMJ0P7gIkPBlfZAnv4sYLi75V4EvZC6maLgvriPfaSB1sgHYXAo1gwZDZD"
 VERIFY_TOKEN = "alfred"
 
+def short_url(url):
+    post_url = 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyB0N1UrT-OxThltr9Lr1bb1IeCuYma-rro'
+    params = json.dumps({'longUrl': url})
+    response = requests.post(post_url,params,headers={'Content-Type': 'application/json'})
+    response1=json.loads(response.text)
+    return response1['id']
+
 class Event(db.Model):
     __tablename__='events'
     id=db.Column(db.Integer,primary_key=True)
     sender_id=db.Column(db.String(100))
-    receipt_id=db.Column(db.String(100))
     name=db.Column(db.String(100),default='event')
     date=db.Column(db.DateTime)
+    reminded=db.Column(db.Boolean,default=False)
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -46,6 +55,26 @@ def webook():
 
                 if messaging_event.get("message"):  # someone sent us a message
 
+                    text1=requests.get('http://api.meaningcloud.com/topics-2.0?key=26f841b83b15255990e9a1cfed9a47a9&of=json&lang=en&ilang=en&txt='+messaging_event["message"]+'&tt=a&uw=y')
+                    textp=json.loads(text1.text)
+                    if textp['time_expression_list']:
+                        for t in textp['time_expression_list']:
+                            rtime = ""
+                            if t['precision'] == "day" or t['precision'] == "weekday":
+                                dates = t['actual_time']
+                                tim = "08:33:54.227806"
+                                rtime = dates
+                                try:
+                                    for e in textp['entity_list']:
+                                        event= e['form']
+                                        eve=Event(sender_id= messaging_event["sender"]["id"],name=event,date=rtime)
+                                except ValueError:
+                                    eve=Event(sender_id= messaging_event["sender"]["id"],date=rtime)
+                                db.session.add(eve)
+                                db.session.commit()
+
+
+
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
@@ -61,6 +90,26 @@ def webook():
                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
                     pass
 
+                while 1:
+                    try:
+                        all_reminders = Event.query.all()
+                        for i in all_reminders:
+                            if not i.reminded:
+                                event_date = i.date
+                                # print event_date
+                                if event_date == '':
+                                    continue
+                                else:
+                                    event_date = datetime.strptime(event_date, "%Y-%m-%d").date()
+                                    nowdate = datetime.now().date()
+                                    a=divmod((event_date-nowdate).days* 86400+ (datetime.utcnow()-current_user.mood_changed).seconds , 60)
+                                    if a[0]<1440 :
+                                        senderid = i.sender_id
+                        #print i.reminprint "chutiya"
+                                        reminder_message = "Upcoming event " + i.reminder + "on" + i.remindertime
+                                        send_message(senderid, reminder_message)
+                    except ValueError:
+                        continue
     return "ok", 200
 
 
