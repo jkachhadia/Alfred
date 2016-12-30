19e93af687da4236a6e3cc1d23e90c45c099142d
Sumedh Nimkarde <blesssumedh@gmail.com>
2016-12-30 11:17:46 +0530

    update app.py

--- app.py
+++ app.py
@@ -68,12 +68,14 @@ def webook():
             for messaging_event in entry["messaging"]:
 
                 if messaging_event.get("message"):  # someone sent us a message
+                    print 'Got message'
                     roll_no = 0
                     user = db.user.find_one({ 'sender_id' : messaging_event["sender"]["id"] })
                     if user is None :
+                        print 'User not found'
                         send_message(messaging_event["sender"]["id"], "Can I know your roll no??")
                         roll_no = 1
-                    elif roll_no == 1:
+                    if roll_no == 1:
                         user = db.user.insert_one({ "user_id" : messaging_event["sender"]["id"],
                                                 "adm_no" : str(messaging_event["message"]["text"])})
                         roll_no = 0
@@ -115,12 +117,12 @@ def send_message(recipient_id, message_text):
         log(r.status_code)
         log(r.text)
 
-@app.route('/<message>', methods=['GET','POST'])
-def mass(message):
-    subs= db.user
-    for user in subs:
-        send_message(user.user_id,message)
-    return "ok", 200
+# @app.route('/<message>', methods=['GET','POST'])
+# def mass(message):
+#     subs= db.user
+#     for user in subs:
+#         send_message(user.user_id,message)
+#     return "ok", 200
 
 
 
